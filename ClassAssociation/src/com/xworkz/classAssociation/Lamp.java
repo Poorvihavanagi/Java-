b7ca370127454dc19a7313928e7fc4c27d8b7414
Poorvi Havanagi <poorvivh2003@gmail.com>
2025-04-02 12:18:03 +0530

    init commit

---
+++ ClassAssociation/src/com/xworkz/classAssociation/Lamp.java
@@ -0,0 +1,20 @@
+package com.xworkz.classAssociation;
+
+public class Lamp
+{
+    private String color;
+    private String material;
+
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public String getMaterial(){
+        return material;
+    }
+    public void setMaterial(String material){
+        this.material=material;
+    }
+}
---
+++ ClassAssociation/src/com/xworkz/classAssociation/Len.java
@@ -0,0 +1,22 @@
+package com.xworkz.classAssociation;
+
+public class Len
+{
+    private String brand;
+    private double price;
+
+
+    public String getBrand(){
+        return brand;
+    }
+    public void setBrand(String brand){
+        this.brand=brand;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price=price;
+    }
+
+}
