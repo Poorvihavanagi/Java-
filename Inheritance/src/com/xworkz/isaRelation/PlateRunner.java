b164266ec0093a8dec71e37ff315adffbb668701
Poorvi Havanagi <poorvivh2003@gmail.com>
2025-04-07 09:27:12 +0530

    init commit

--- Inheritance/src/com/xworkz/isaRelation/PlateRunner.java
+++ Inheritance/src/com/xworkz/isaRelation/PlateRunner.java
@@ -7,15 +7,20 @@ public class PlateRunner
 {
     public static void main(String[] args) {
         Plate plate=new Plate();
-        plate.serveFood();
-        plate.clean();
-        plate.stack();
+        plate.wash();
+        plate.store();
+        plate.use();
         plate.checkMaterial();
         plate.dry();
+
+        System.out.println("-------------------------------------------------------");
 
         Dish dish = new Plate();
         dish.wash();
         dish.store();
+        dish.dry();
+        dish.use();
+        dish.checkMaterial();
 
     }
 }
